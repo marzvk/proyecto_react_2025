039a262bced521fb5f4ec2199829768107ab66b6
marzvk <marianozovak1@gmail.com>
2025-06-02 16:54:01 -0300

    primer commit entrega-1

---
+++ react_2025/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.naveg}>
+      <div className={styles.superior}>
+        <img src="/logop.png" alt="" className={styles.logo} />
+        <input type="text" placeholder="Buscar" className={styles.buscador} />
+      </div>
+      <ul className={styles.lista}>
+        <li className={styles.listText} >Categorias</li>
+        <li className={styles.listText} >Ofertas</li>
+        <li className={styles.listText} >Registrate</li>
+        <li>
+          <img className={styles.carrito} src="/icons8-carrito-de-compras-50.png" alt="" />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ react_2025/src/components/ProductCard.tsx
@@ -0,0 +1,45 @@
+import styles from "./ProductCard.module.css";
+
+export type ProductCardProps = {
+  src: string;
+  title: string;
+  precio: number;
+  cuotas?: number;
+  descuento?: number;
+  anio?: string;
+  ubicacion?: string;
+  envio?: string;
+  km?: number;
+};
+
+// parametro props tiene el type ProductCardProps
+function FrontShowElement(props: ProductCardProps) {
+  const {
+    src,
+    title,
+    precio,
+    cuotas = 0,
+    ubicacion,
+    anio ,
+    descuento = 0,
+    envio,
+    km,
+  } = props;
+  return (
+    <article className={styles.card}>
+      <img className={styles.img} src={src} alt="" />
+      <h4 className={styles.titulo}>{title}</h4>
+      <h2>$ {precio.toLocaleString()}</h2>
+      {cuotas > 0 && <p>{cuotas} cuotas</p>}
+      {descuento > 0 && <p className={styles.desc}>{descuento} % Off</p>}
+      <p>{ubicacion}</p>
+      <p className={styles.envio}> {envio}</p>
+      <h5>
+        {anio}
+        {km !== undefined && ` | ${km.toLocaleString()} km`}
+      </h5>
+    </article>
+  );
+}
+
+export default FrontShowElement;
